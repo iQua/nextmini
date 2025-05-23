90bf2fe4a3e037ea2e1c802b9719d88baab36381
Baochun Li <bli@ece.toronto.edu>
2025-05-22 22:03:56 -0400

    Fixed issues in PyTorch example's Dockerfile.

--- examples/pytorch/train.sh
+++ examples/pytorch/train.sh
@@ -1,1 +1,1 @@
-mpirun --allow-run-as-root -np 4 -H node1:1,node2:1,node3:1,node4:1 -x MASTER_ADDR=node1 -x MASTER_PORT=1234 -x PATH -bind-to none -map-by slot sh -c 'export RANK=$OMPI_COMM_WORLD_RANK; export WORLD_SIZE=$OMPI_COMM_WORLD_SIZE; uv run lenet5.py'
+mpirun --allow-run-as-root -np 4 -H 10.0.0.1:1,10.0.0.2:1,10.0.0.3:1,10.0.0.4:1 -x MASTER_ADDR=node1 -x MASTER_PORT=1234 -x PATH -bind-to none -map-by slot sh -c 'export RANK=$OMPI_COMM_WORLD_RANK; export WORLD_SIZE=$OMPI_COMM_WORLD_SIZE; uv run lenet5.py'
