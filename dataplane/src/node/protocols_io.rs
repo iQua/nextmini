82fe0e0d7f255937512b03950ebfd08d76f9e4dd
Xindan Zhang <winifredwsig@outlook.com>
2025-05-27 22:09:36 -0400

    Refactored ProtocolWriter to use actor pattern.

--- dataplane/src/node/protocols_io.rs
+++ dataplane/src/node/protocols_io.rs
@@ -1,11 +1,9 @@
-use crate::dataplane::PacketBuf;
-use s2n_quic::stream::{ReceiveStream, SendStream};
-use std::sync::Arc;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio::io::{ReadHalf, WriteHalf};
-use tokio::net::TcpStream;
-use tokio::net::UdpSocket;
-use tokio::sync::Mutex;
+use crate::node::PacketBuf;
+use crate::node::quic::{QuicReader, QuicWriter};
+use crate::node::tcp::{TcpReader, TcpWriter};
+use crate::node::udp::{UdpReader, UdpWriter};
+use tokio::sync::mpsc;
+use tracing::error;
 
 pub enum ProtocolReader {
     Tcp(TcpReader),
@@ -23,26 +21,127 @@ impl ProtocolReader {
     }
 }
 
-pub enum ProtocolWriter {
-    Tcp(TcpWriter),
-    Udp(UdpWriter),
-    Quic(QuicWriter),
+enum ProtocolWriterMessage {
+    Send(Vec<u8>),
+    Shutdown,
 }
 
-impl ProtocolWriter {
-    pub async fn send(&mut self, data: &[u8]) {
-        match self {
-            Self::Tcp(writer) => writer.send(data).await,
-            Self::Udp(writer) => writer.send(data).await,
-            Self::Quic(writer) => writer.send(data).await,
+struct TcpProtocolWriter {
+    receiver: mpsc::Receiver<ProtocolWriterMessage>,
+    writer: TcpWriter,
+}
+
+impl TcpProtocolWriter {
+    async fn run(&mut self) {
+        while let Some(message) = self.receiver.recv().await {
+            match message {
+                ProtocolWriterMessage::Send(data) => {
+                    self.writer.send(&data).await;
+                }
+                ProtocolWriterMessage::Shutdown => break,
+            }
+        }
+    }
+}
+
+struct UdpProtocolWriter {
+    receiver: mpsc::Receiver<ProtocolWriterMessage>,
+    writer: UdpWriter,
+}
+
+impl UdpProtocolWriter {
+    async fn run(&mut self) {
+        while let Some(message) = self.receiver.recv().await {
+            match message {
+                ProtocolWriterMessage::Send(data) => {
+                    self.writer.send(&data).await;
+                }
+                ProtocolWriterMessage::Shutdown => break,
+            }
+        }
+    }
+}
+
+struct QuicProtocolWriter {
+    receiver: mpsc::Receiver<ProtocolWriterMessage>,
+    writer: QuicWriter,
+}
+
+impl QuicProtocolWriter {
+    async fn run(&mut self) {
+        while let Some(message) = self.receiver.recv().await {
+            match message {
+                ProtocolWriterMessage::Send(data) => {
+                    self.writer.send(&data).await;
+                }
+                ProtocolWriterMessage::Shutdown => break,
+            }
+        }
+    }
+}
+
+#[derive(Clone)]
+pub struct ProtocolWriterHandle {
+    sender: mpsc::Sender<ProtocolWriterMessage>,
+}
+
+impl ProtocolWriterHandle {
+    pub fn new_tcp(tcp_writer: TcpWriter) -> Self {
+        let (sender, receiver) = mpsc::channel(100);
+
+        let mut actor = TcpProtocolWriter {
+            receiver,
+            writer: tcp_writer,
+        };
+
+        tokio::spawn(async move {
+            actor.run().await;
+        });
+
+        Self { sender }
+    }
+
+    pub fn new_udp(udp_writer: UdpWriter) -> Self {
+        let (sender, receiver) = mpsc::channel(100);
+
+        let mut actor = UdpProtocolWriter {
+            receiver,
+            writer: udp_writer,
+        };
+
+        tokio::spawn(async move {
+            actor.run().await;
+        });
+
+        Self { sender }
+    }
+
+    pub fn new_quic(quic_writer: QuicWriter) -> Self {
+        let (sender, receiver) = mpsc::channel(100);
+
+        let mut actor = QuicProtocolWriter {
+            receiver,
+            writer: quic_writer,
+        };
+
+        tokio::spawn(async move {
+            actor.run().await;
+        });
+
+        Self { sender }
+    }
+
+    pub async fn send(&self, data: &[u8]) {
+        if let Err(_) = self
+            .sender
+            .send(ProtocolWriterMessage::Send(data.to_vec()))
+            .await
+        {
+            error!("Failed to send data through protocol writer channel");
         }
     }
 
     pub fn reproduce(&self) -> Self {
-        match self {
-            Self::Tcp(writer) => Self::Tcp(writer.reproduce()),
-            Self::Udp(writer) => Self::Udp(writer.reproduce()),
-            Self::Quic(writer) => Self::Quic(writer.reproduce()),
-        }
+        self.clone()
     }
 }
