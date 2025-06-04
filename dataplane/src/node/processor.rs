ff20612f583821267d859bbfee8d8afa1f28e9ac
Baochun Li <bli@ece.toronto.edu>
2025-06-04 11:50:42 -0400

    Removed another unused import.

--- dataplane/src/node/processor.rs
+++ dataplane/src/node/processor.rs
@@ -15,7 +15,7 @@ use crate::node::config::LocalConfig;
 use crate::node::local_interface::LocalInterfaceHandle;
 use crate::node::packet::Packet;
 use crate::node::route::RoutingTable;
-use crate::node::scheduler::{SchedulerHandle, SchedulerMessage};
+use crate::node::scheduler::SchedulerHandle;
 use crate::node::{FlowId, NodeId};
 
 // Message types for the processor actor.
