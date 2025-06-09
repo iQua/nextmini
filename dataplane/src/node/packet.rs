43a867b64ffdcf3588e95e853fd6e334cc4231ac
Baochun Li <bli@ece.toronto.edu>
2025-06-09 01:45:40 -0400

    Simplified Pakcet::get_flow_id_from_buf().

--- dataplane/src/node/packet.rs
+++ dataplane/src/node/packet.rs
@@ -1,6 +1,4 @@
-use std::io::Cursor;
-
-use byteorder::{BigEndian, ReadBytesExt};
+use byteorder::{BigEndian, ByteOrder};
 
 use crate::node::{FlowId, PacketBuf};
 
@@ -22,15 +20,8 @@ impl Packet {
 
     fn get_flow_id_from_buf(buf: &PacketBuf) -> FlowId {
         if buf[0] >> 4 == 4 {
-            // IPv4 packet detected, now extracts source and destination IP addresses
-            let mut cursor = Cursor::new(buf.get(12..20).unwrap());
-            let src_dst_ip = cursor.read_u64::<BigEndian>().unwrap();
-
-            let mut cursor = Cursor::new(buf.get(20..24).unwrap());
-            let src_dst_port = cursor.read_u32::<BigEndian>().unwrap();
-
-            // packs complete 4-tuple into 128-bit flow_id without compression:
-            // src_ip(32) + dst_ip(32) + src_port(16) + dst_port(16) + reserved(32)
+            let src_dst_ip = BigEndian::read_u64(&buf[12..20]);
+            let src_dst_port = BigEndian::read_u32(&buf[20..24]);
             (src_dst_ip as u128) << 64 | (src_dst_port as u128) << 32
         } else {
             0
