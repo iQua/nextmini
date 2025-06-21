2b8ac6abf56939f3d606ae0694c8f2ca46dfb208
ShengwenChang <shengwen.chang@mail.utoronto.ca>
2025-06-20 20:48:13 -0400

    Added flow_id conversion method.

--- dataplane/src/node/mod.rs
+++ dataplane/src/node/mod.rs
@@ -80,3 +80,30 @@ impl FlowIdExt for FlowId {
         hash as usize
     }
 }
+
+pub trait FlowCvt {
+    fn from_ip_port_to_flow_id(
+        &self,
+        src_ip: u32,
+        dst_ip: u32,
+        src_port: u16,
+        dst_port: u16,
+    ) -> FlowId;
+}
+
+impl FlowCvt for FlowId {
+    fn from_ip_port_to_flow_id(
+        &self,
+        src_ip: u32,
+        dst_ip: u32,
+        src_port: u16,
+        dst_port: u16,
+    ) -> FlowId {
+        let mut flow_id = 0;
+        flow_id |= (src_ip as u128) << 96;
+        flow_id |= (dst_ip as u128) << 64;
+        flow_id |= (src_port as u128) << 48;
+        flow_id |= (dst_port as u128) << 32;
+        flow_id
+    }
+}
