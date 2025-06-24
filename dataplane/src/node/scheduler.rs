551a7839f4c6667986132413ccbb2176ee86001b
ShengwenChang <shengwen.chang@mail.utoronto.ca>
2025-06-23 22:25:46 -0400

    Added threshold judgement.

--- dataplane/src/node/scheduler.rs
+++ dataplane/src/node/scheduler.rs
@@ -378,32 +378,26 @@ impl SchedulerQueue for WrrQueue {
     fn collect_packets(&self, batch: &mut Vec<Packet>) {
         let flow_queues = self.flow_queues.read().unwrap();
         let flow_weights = self.flow_weights.read().unwrap();
+        let flow_ids: Vec<FlowId> = flow_queues.keys().cloned().collect();
 
-        let mut min_rounds: Option<usize> = None;
-        let mut flow_ids: Vec<FlowId> = Vec::new();
-
-        for (flow_id, queue) in flow_queues.iter() {
-            if !queue.is_empty() {
+        // The minimum number of rounds to empty one of the queues by WRR
+        let min_rounds = flow_queues
+            .iter()
+            .filter_map(|(flow_id, queue)| {
                 let weight = *flow_weights.get(flow_id).unwrap_or(&1);
                 let rounds = queue.len() / weight;
-                min_rounds = match min_rounds {
-                    Some(current_min_rounds) => Some(rounds.min(current_min_rounds)), // find the minimum number of rounds
-                    None => Some(rounds), // set the minimum number of rounds the first time
-                };
+                if rounds > 0 { Some(rounds) } else { None }
+            })
+            .min()
+            .unwrap_or(0);
 
-                // Get the non-empty queues' flow_ids
-                flow_ids.push(*flow_id);
-            }
-        }
-
-        // If there is no non-empty queue, return
-        if let None = min_rounds {
+        if min_rounds == 0 {
             return;
         }
 
         drop(flow_queues);
 
-        for _ in 0..min_rounds.unwrap() {
+        for _ in 0..min_rounds {
             for flow_id in &flow_ids {
                 let flow_weights = self.flow_weights.read().unwrap();
                 let flow_queues = self.flow_queues.read().unwrap();
@@ -419,7 +413,20 @@ impl SchedulerQueue for WrrQueue {
             }
         }
 
-        // Make judgement here
+        // If remaining packets accumulated in the queues are more than what this round can handle, backlogging occurs
+        // So, we need to send them all
+        let flow_queues = self.flow_queues.read().unwrap();
+        let flow_weights = self.flow_weights.read().unwrap();
+        for (flow_id, flow_queue) in flow_queues.iter() {
+            let weight = *flow_weights.get(flow_id).unwrap_or(&1);
+            if flow_queue.len() > min_rounds * weight {
+                for _ in 0..weight {
+                    if let Some(packet) = flow_queue.pop() {
+                        batch.push(packet);
+                    }
+                }
+            }
+        }
     }
     fn is_empty(&self) -> bool {
         let flow_queues = self.flow_queues.read().unwrap();
