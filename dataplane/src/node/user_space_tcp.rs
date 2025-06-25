4b8bb9abf0a7fff8d9bd33fb2e0ee7ce8008f1cd
Xindan Zhang <xindan.zhang@mail.utoronto.ca>
2025-06-25 02:01:58 -0400

    Removed redundant code.

--- dataplane/src/node/user_space_tcp.rs
+++ dataplane/src/node/user_space_tcp.rs
@@ -1,22 +1,21 @@
-use std::cmp;
 use std::net::Ipv4Addr;
 use std::thread;
-use std::time::Instant as StdInstant;
 
 use flume;
 use smoltcp::iface::{Config, Interface, SocketSet};
-use smoltcp::phy::{Device, DeviceCapabilities, Medium};
-use smoltcp::socket::tcp;
 use smoltcp::time::{Duration, Instant};
 use smoltcp::wire::{IpAddress, IpCidr};
-use tracing::{error, info};
+use tracing::error;
+
+use crate::node::config::LocalConfig;
+use crate::node::user_space_tcp_client::UserSpaceTcpClient;
+use crate::node::user_space_tcp_server::UserSpaceTcpServer;
+use crate::node::user_space_tcp_utils::VirtualDevice;
 
 use crate::node::LocalDestination;
-use crate::node::NodeIdExt;
-use crate::node::config::LocalConfig;
 use crate::node::packet::Packet;
 use crate::node::processor::ProcessorHandle;
-use nextmini_messages::{Flow, FlowSpec};
+use nextmini_messages::Flow;
 
 #[derive(Clone, Debug)]
 pub struct UserSpaceTcpSource {
@@ -25,15 +24,6 @@ pub struct UserSpaceTcpSource {
     processor_handle: ProcessorHandle,
     packet_sender: flume::Sender<Packet>,
     packet_receiver: flume::Receiver<Packet>,
-}
-
-#[derive(Debug, Clone)]
-struct ConnectionState {
-    connected: bool,
-    start_time: StdInstant,
-    time_last_updated: StdInstant,
-    bytes_last_updated: u64,
-    bytes_total: u64,
 }
 
 impl UserSpaceTcpSource {
@@ -71,254 +61,44 @@ impl UserSpaceTcpSource {
                 .unwrap();
         });
 
-        // socket buffer 655350 by default
-        const SOCKET_BUFFER_SIZE: usize = 655350;
-
-        // connects to a remote endpoint
-        // Creates the TCP socket set
+        // creates the TCP socket set
         let mut sockets = SocketSet::new(vec![]);
 
-        // first, creates server sockets based on the number of inbound flows
+        // creates server sockets based on the number of inbound flows
         // TODO: thinks of a new design
-        let mut server_handles = Vec::new();
         let incoming_flows: Vec<_> = flows
             .iter()
             .filter(|f| f.dst_node_id == self.config.node_id)
+            .cloned() // gets the ownership of Flow
             .collect();
-
-        for i in 0..incoming_flows.len() {
-            let server_rx_buffer = tcp::SocketBuffer::new(vec![0; SOCKET_BUFFER_SIZE]);
-            let server_tx_buffer = tcp::SocketBuffer::new(vec![0; SOCKET_BUFFER_SIZE]);
-            let server_socket = tcp::Socket::new(server_rx_buffer, server_tx_buffer);
-            let server_handle = sockets.add(server_socket);
-            server_handles.push(server_handle);
-            info!("Created server socket {}", i);
-        }
 
         let node_id = self.config.node_id;
 
-        let client_flow_configs: Vec<_> = flows
+        let outgoing_flows: Vec<_> = flows
             .iter()
             .filter(|f| f.src_node_id == node_id)
             .cloned()
             .collect();
 
-        let mut client_handles = Vec::new();
-        for (i, _flow) in client_flow_configs.iter().enumerate() {
-            let client_rx_buffer = tcp::SocketBuffer::new(vec![0; SOCKET_BUFFER_SIZE]);
-            let client_tx_buffer = tcp::SocketBuffer::new(vec![0; SOCKET_BUFFER_SIZE]);
-            let client_socket = tcp::Socket::new(client_rx_buffer, client_tx_buffer);
-            let client_handle = sockets.add(client_socket);
-            client_handles.push(client_handle);
-            info!("Created client socket {} for an outgoing flow.", i);
-        }
-
-        let base_server_port = self.config.user_space_server_port;
-
         // spawns a new thread as smoltcp is not designed to use async Rust and Tokio
         thread::spawn(move || {
             let mut device = device;
 
-            let incoming_flows: Vec<_> =
-                flows.iter().filter(|f| f.dst_node_id == node_id).collect();
-            let outgoing_flows: Vec<_> =
-                flows.iter().filter(|f| f.src_node_id == node_id).collect();
+            let mut server =
+                UserSpaceTcpServer::new(config_clone.clone(), incoming_flows, &mut sockets);
 
-            let mut server_states: Vec<ConnectionState> = (0..server_handles.len())
-                .map(|_i| ConnectionState {
-                    connected: false,
-                    start_time: StdInstant::now(),
-                    time_last_updated: StdInstant::now(),
-                    bytes_last_updated: 0,
-                    bytes_total: 0,
-                })
-                .collect();
-
-            let mut client_states: Vec<ConnectionState> = outgoing_flows
-                .iter()
-                .map(|_f| ConnectionState {
-                    connected: false,
-                    start_time: StdInstant::now(),
-                    time_last_updated: StdInstant::now(),
-                    bytes_last_updated: 0,
-                    bytes_total: 0,
-                })
-                .collect();
-
-            let mut server_listening = vec![false; server_handles.len()];
-            let mut client_connecting = vec![false; client_handles.len()];
+            let mut client =
+                UserSpaceTcpClient::new(config_clone.clone(), outgoing_flows, &mut sockets);
 
             loop {
                 let timestamp = Instant::now();
+
                 iface.poll(timestamp, &mut device, &mut sockets);
 
-                for (i, &server_handle) in server_handles.iter().enumerate() {
-                    let socket = sockets.get_mut::<tcp::Socket>(server_handle);
-
-                    if !socket.is_active() && !socket.is_listening() && !server_listening[i] {
-                        match socket.listen(base_server_port) {
-                            Ok(_) => {
-                                info!("Server {} listening on port {}", i, base_server_port);
-                                server_listening[i] = true;
-                            }
-                            Err(e) => {
-                                error!("Server {} failed to listen: {:?}", i, e);
-                            }
-                        }
-                    }
-
-                    if socket.is_active() {
-                        if !server_states[i].connected {
-                            server_states[i].connected = true;
-                            info!("Server {} accepted connection", i);
-                        }
-
-                        if socket.can_recv() {
-                            match socket.recv(|buffer| {
-                                let len = buffer.len();
-                                (len, len)
-                            }) {
-                                Ok(received) if received > 0 => {
-                                    if server_states[i].bytes_total == 0 {
-                                        server_states[i].start_time = StdInstant::now();
-                                        info!("Server {} started receiving data", i);
-                                    }
-
-                                    server_states[i].bytes_total += received as u64;
-                                    server_states[i].bytes_last_updated += received as u64;
-                                    let now = StdInstant::now();
-
-                                    let elapsed_time = now
-                                        .duration_since(server_states[i].time_last_updated)
-                                        .as_secs_f64();
-                                    if elapsed_time > 1.0 {
-                                        let throughput =
-                                            (server_states[i].bytes_last_updated as f64 * 8.0)
-                                                / (elapsed_time * 1_000_000_000.0);
-
-                                        println!(
-                                            "Server {} throughput: {:.3} Gbps ({} bytes in {:.3}s)",
-                                            i,
-                                            throughput,
-                                            server_states[i].bytes_last_updated,
-                                            elapsed_time
-                                        );
-
-                                        server_states[i].bytes_last_updated = 0;
-                                        server_states[i].time_last_updated = now;
-                                    }
-
-                                    if incoming_flows[i].flow_size.exceeded(
-                                        server_states[i].bytes_total,
-                                        server_states[i].start_time,
-                                    ) {
-                                        println!("Server received all the flows.");
-                                        socket.close();
-                                    }
-                                }
-                                Err(e) => {
-                                    info!("Server {} recv error: {:?}", i, e);
-                                }
-                                Ok(_) => {}
-                            }
-                        }
-                    }
-                }
-
-                for (i, &client_handle) in client_handles.iter().enumerate() {
-                    let flow = &outgoing_flows[i];
-                    let socket = sockets.get_mut::<tcp::Socket>(client_handle);
-                    let cx = iface.context();
-
-                    if !socket.is_open() && !client_connecting[i] {
-                        let remote_addr = IpAddress::from(flow.dst_node_id.ip_addr(
-                            config_clone.user_space_base_addr,
-                            config_clone.local_netmask,
-                        ));
-                        let remote_endpoint = (remote_addr, base_server_port as u16);
-                        let client_port = config_clone.user_space_client_port + i as u16;
-
-                        match socket.connect(cx, remote_endpoint, client_port) {
-                            Ok(_) => {
-                                info!(
-                                    "Client {} connecting from port {} to {}:{}",
-                                    i, client_port, remote_addr, base_server_port
-                                );
-                                client_connecting[i] = true;
-                            }
-                            Err(e) => {
-                                error!("Client {} connect error: {:?}", i, e);
-                            }
-                        }
-                    }
-
-                    if socket.is_active() {
-                        if !client_states[i].connected {
-                            client_states[i].connected = true;
-                            info!("Client {} connected successfully", i);
-                        }
-
-                        // sends data
-                        if socket.can_send()
-                            && !outgoing_flows[i]
-                                .flow_size
-                                .exceeded(client_states[i].bytes_total, client_states[i].start_time)
-                        {
-                            let remaining =
-                                if let FlowSpec::Bytes(size) = outgoing_flows[i].flow_size {
-                                    size as u64 - client_states[i].bytes_total
-                                } else {
-                                    // For duration-based flows, we can send as much as the buffer allows.
-                                    // The check for finishing is handled by `exceeded`.
-                                    SOCKET_BUFFER_SIZE as u64
-                                };
-
-                            match socket.send(|buf| {
-                                let to_send = cmp::min(buf.len(), remaining as usize);
-                                buf[..to_send].fill(0xAA);
-                                (to_send, to_send)
-                            }) {
-                                Ok(sent) if sent > 0 => {
-                                    client_states[i].bytes_total += sent as u64;
-                                    client_states[i].bytes_last_updated += sent as u64;
-                                    let now = StdInstant::now();
-
-                                    let elapsed_time = now
-                                        .duration_since(client_states[i].time_last_updated)
-                                        .as_secs_f64();
-                                    if elapsed_time > 1.0 {
-                                        let throughput =
-                                            (client_states[i].bytes_last_updated as f64 * 8.0)
-                                                / (elapsed_time * 1_000_000_000.0);
-
-                                        println!(
-                                            "Client {} throughput: {:.3} Gbps ({} bytes in {:.3}s)",
-                                            i,
-                                            throughput,
-                                            client_states[i].bytes_last_updated,
-                                            elapsed_time
-                                        );
-
-                                        client_states[i].bytes_last_updated = 0;
-                                        client_states[i].time_last_updated = now;
-                                    }
-
-                                    if outgoing_flows[i].flow_size.exceeded(
-                                        client_states[i].bytes_total,
-                                        client_states[i].start_time,
-                                    ) {
-                                        println!("Client sent all the flows.");
-                                        socket.close();
-                                    }
-                                }
-                                Err(e) => {
-                                    error!("Client {} send error: {:?}", i, e);
-                                }
-                                Ok(_) => {}
-                            }
-                        }
-                    }
-                }
+                // starts listening from client
+                server.process(&mut sockets);
+                // starts connecting to server
+                client.process(&mut sockets, iface.context());
 
                 match iface.poll_delay(timestamp, &sockets) {
                     Some(Duration::ZERO) => {
@@ -344,64 +124,5 @@ impl LocalDestination for UserSpaceTcpSource {
         if let Err(e) = self.packet_sender.try_send(packet) {
             error!("Failed to send packet to user-space TCP source: {:?}", e);
         }
-    }
-}
-
-#[derive(Clone)]
-struct VirtualDevice {
-    config: LocalConfig,
-    receiver: flume::Receiver<Packet>,
-    sender: ProcessorHandle,
-}
-
-impl Device for VirtualDevice {
-    type RxToken<'a> = PacketRxToken;
-    type TxToken<'a> = PacketTxToken;
-
-    fn receive(&mut self, _timestamp: Instant) -> Option<(Self::RxToken<'_>, Self::TxToken<'_>)> {
-        self.receiver
-            .try_recv()
-            .ok()
-            .map(|packet| (PacketRxToken(packet), PacketTxToken(self.sender.clone())))
-    }
-
-    fn transmit(&mut self, _timestamp: Instant) -> Option<Self::TxToken<'_>> {
-        Some(PacketTxToken(self.sender.clone()))
-    }
-
-    fn capabilities(&self) -> DeviceCapabilities {
-        let mut caps = DeviceCapabilities::default();
-        caps.medium = Medium::Ip; // needs IP packet format
-        caps.max_transmission_unit = self.config.mtu as usize;
-        caps
-    }
-}
-
-struct PacketRxToken(Packet);
-
-impl smoltcp::phy::RxToken for PacketRxToken {
-    fn consume<R, F>(self, f: F) -> R
-    where
-        F: FnOnce(&[u8]) -> R,
-    {
-        f(&self.0.buf[0..self.0.packet_size])
-    }
-}
-
-struct PacketTxToken(ProcessorHandle);
-
-impl smoltcp::phy::TxToken for PacketTxToken {
-    fn consume<R, F>(self, len: usize, f: F) -> R
-    where
-        F: FnOnce(&mut [u8]) -> R,
-    {
-        let mut buf = vec![0; len];
-        let result = f(&mut buf);
-        let packet = Packet::new(len, buf);
-
-        // uses non-blocking send() to send the outbound packet
-        self.0.process_packet(packet);
-
-        result
     }
 }
