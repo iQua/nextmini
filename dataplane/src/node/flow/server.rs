7f509bc7168518cc95851414778f281a72df44b3
Xindan Zhang <xindan.zhang@mail.utoronto.ca>
2025-06-28 00:55:13 -0400

    Spawned a thread and TCP server instance for each flow.

--- dataplane/src/node/flow/server.rs
+++ dataplane/src/node/flow/server.rs
@@ -1,7 +1,7 @@
 use std::thread;
 use std::time::Instant as StdInstant;
 
-use smoltcp::iface::{Config, Interface, SocketHandle, SocketSet};
+use smoltcp::iface::{Config, Interface, SocketSet};
 use smoltcp::socket::tcp;
 use smoltcp::time::Instant;
 use smoltcp::wire::{HardwareAddress, IpAddress, IpCidr};
@@ -21,100 +21,75 @@ const SOCKET_BUFFER_SIZE: usize = 655350;
 // still handles multiple sockets for a single thread for now.
 #[derive(Clone, Debug)]
 pub struct UserSpaceServerHandle {
-    flow_sender: flume::Sender<Vec<Flow>>,
     packet_sender: flume::Sender<Packet>,
+    packet_receiver: flume::Receiver<Packet>,
+    processor_handle: ProcessorHandle,
+    config: LocalConfig,
 }
 
 impl UserSpaceServerHandle {
     pub fn new(config: LocalConfig, processor_handle: ProcessorHandle) -> Self {
-        let (flow_sender, flow_receiver) = flume::unbounded();
         let (packet_sender, packet_receiver) = flume::bounded(config.channel_capacity);
-
-        // spawns a thread
-        thread::spawn(move || {
-            let server =
-                UserSpaceTcpServer::new(config, processor_handle, flow_receiver, packet_receiver);
-
-            server.start();
-        });
 
         info!("Server handle created and thread started.");
 
         Self {
-            flow_sender,
             packet_sender,
+            packet_receiver,
+            processor_handle,
+            config,
         }
     }
 
-    pub fn add_flows(&self, flows: Vec<Flow>) -> Result<(), String> {
-        self.flow_sender
-            .send(flows)
-            .map_err(|e| format!("Failed to send flow to server: {}", e))
+    pub fn add_flows(&self, flows: Vec<Flow>) {
+        for flow in flows {
+            let config = self.config.clone();
+            let processor_handle = self.processor_handle.clone();
+            let packet_receiver = self.packet_receiver.clone();
+
+            // spawns a thread
+            thread::spawn(move || {
+                let server =
+                    UserSpaceTcpServer::new(config, flow, processor_handle, packet_receiver);
+                server.start();
+            });
+        }
     }
 }
 
 struct UserSpaceTcpServer {
     config: LocalConfig,
+    flow: Flow,
     processor_handle: ProcessorHandle,
-    flow_receiver: flume::Receiver<Vec<Flow>>,
     packet_receiver: flume::Receiver<Packet>,
-    handles: Vec<SocketHandle>,
-    states: Vec<ConnectionState>,
-    listening: Vec<bool>,
-    flows: Vec<Flow>,
+    state: ConnectionState,
+    listening: bool,
 }
 
 impl UserSpaceTcpServer {
     fn new(
         config: LocalConfig,
+        flow: Flow,
         processor_handle: ProcessorHandle,
-        flow_receiver: flume::Receiver<Vec<Flow>>,
         packet_receiver: flume::Receiver<Packet>,
     ) -> Self {
         info!("Creating user-space TCP server.");
 
-        let states = Vec::new();
-        let listening = Vec::new();
+        let state = ConnectionState {
+            connected: false,
+            start_time: StdInstant::now(),
+            time_last_updated: StdInstant::now(),
+            bytes_last_updated: 0,
+            bytes_total: 0,
+        };
 
         Self {
             config,
+            flow,
             processor_handle,
-            flow_receiver,
             packet_receiver,
-            handles: Vec::new(),
-            states,
-            listening,
-            flows: Vec::new(),
-        }
-    }
-
-    // adds new incoming flows to the server
-    pub fn add_flows(&mut self, sockets: &mut SocketSet) {
-        while let Ok(flows) = self.flow_receiver.try_recv() {
-            for flow in flows {
-                let i = self.handles.len();
-                let server_rx_buffer = tcp::SocketBuffer::new(vec![0; SOCKET_BUFFER_SIZE]);
-                let server_tx_buffer = tcp::SocketBuffer::new(vec![0; SOCKET_BUFFER_SIZE]);
-
-                let server_socket = tcp::Socket::new(server_rx_buffer, server_tx_buffer);
-
-                let server_handle = sockets.add(server_socket);
-                self.handles.push(server_handle);
-
-                info!("Created server socket {}.", i);
-
-                self.states.push(ConnectionState {
-                    connected: false,
-                    start_time: StdInstant::now(),
-                    time_last_updated: StdInstant::now(),
-                    bytes_last_updated: 0,
-                    bytes_total: 0,
-                });
-                self.listening.push(false);
-                self.flows.push(flow);
-
-                info!("Added new server socket {} for incoming flow.", i);
-            }
+            state,
+            listening: false,
         }
     }
 
@@ -143,72 +118,80 @@ impl UserSpaceTcpServer {
 
         let mut sockets = SocketSet::new(vec![]);
 
+        let server_rx_buffer = tcp::SocketBuffer::new(vec![0; SOCKET_BUFFER_SIZE]);
+        let server_tx_buffer = tcp::SocketBuffer::new(vec![0; SOCKET_BUFFER_SIZE]);
+
+        let server_socket = tcp::Socket::new(server_rx_buffer, server_tx_buffer);
+        let server_handle = sockets.add(server_socket);
+
         info!("User-space TCP server initialized, starting main loop.");
 
         loop {
             let timestamp = Instant::now();
 
             iface.poll(timestamp, &mut device, &mut sockets);
-            self.add_flows(&mut sockets);
-            self.receive_data(&mut sockets);
+
+            let socket = sockets.get_mut::<tcp::Socket>(server_handle);
+            self.receive_data(socket);
         }
     }
 
-    fn receive_data(&mut self, sockets: &mut SocketSet) {
+    fn receive_data(&mut self, socket: &mut tcp::Socket) {
         // receives data from the sockets
         // gets the base server port
         let base_server_port = self.config.user_space_server_port;
 
-        for (i, &server_handle) in self.handles.iter().enumerate() {
-            // gets a mutable reference to the socket
-            let socket = sockets.get_mut::<tcp::Socket>(server_handle);
-
-            // Start listening
-            if !socket.is_active() && !socket.is_listening() && !self.listening[i] {
-                // listens on the base server port
-                match socket.listen(base_server_port) {
-                    Ok(_) => {
-                        info!("Server {} listening on port {}", i, base_server_port);
-                        // sets the listening state to true
-                        self.listening[i] = true;
-                    }
-                    Err(e) => {
-                        error!("Server {} failed to listen: {:?}", i, e);
-                    }
+        // Start listening
+        if !socket.is_active() && !socket.is_listening() && !self.listening {
+            // listens on the base server port
+            match socket.listen(base_server_port) {
+                Ok(_) => {
+                    info!("Server listening on port {}", base_server_port);
+                    // sets the listening state to true
+                    self.listening = true;
+                }
+                Err(e) => {
+                    error!("Server failed to listen: {:?}", e);
                 }
             }
+        }
 
-            // if the socket is active and haven't connected
-            if socket.is_active() {
-                if !self.states[i].connected {
-                    self.states[i].connected = true;
-                    info!("Server {} accepted connection", i);
-                }
+        // if the socket is active and haven't connected
+        if socket.is_active() {
+            if !self.state.connected {
+                self.state.connected = true;
+                info!("Server accepted connection");
+            }
 
-                // checks if there is data to receive
-                if socket.can_recv() {
-                    // receives data from the socket
-                    match socket.recv(|buf| {
-                        let len = buf.len();
-                        (len, len)
-                    }) {
-                        // if data received, print out the throughput
-                        Ok(received) if received > 0 => {
-                            self.states[i].test_throughput(i, self.flows[i].src_node_id, None, received as u64);
+            // checks if there is data to receive
+            if socket.can_recv() {
+                // receives data from the socket
+                match socket.recv(|buf| {
+                    let len = buf.len();
+                    (len, len)
+                }) {
+                    // if data received, print out the throughput
+                    Ok(received) if received > 0 => {
+                        self.state.test_throughput(
+                            self.flow.src_node_id,
+                            self.flow.src_node_id,
+                            None,
+                            received as u64,
+                        );
 
-                            if self.flows[i]
-                                .flow_size
-                                .exceeded(self.states[i].bytes_total, self.states[i].start_time)
-                            {
-                                info!("Server {} received all flow data", i);
-                                socket.close();
-                            }
+                        if self
+                            .flow
+                            .flow_size
+                            .exceeded(self.state.bytes_total, self.state.start_time)
+                        {
+                            info!("Server received all flow data");
+                            socket.close();
                         }
-                        Err(e) => {
-                            error!("Server {} receives error: {:?}.", i, e);
-                        }
-                        Ok(_) => {}
                     }
+                    Err(e) => {
+                        error!("Server receives error: {:?}.", e);
+                    }
+                    Ok(_) => {}
                 }
             }
         }
