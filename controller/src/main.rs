a3cd6cca124c36a23fa9f0ee21806557a08d3648
Xindan Zhang <xindan.zhang@mail.utoronto.ca>
2025-06-29 16:46:39 -0400

    Added logic for connected node count on new node connection.

--- controller/src/main.rs
+++ controller/src/main.rs
@@ -194,6 +194,16 @@ async fn handle_connection(
 
                         current_node_id = Some(node_id);
 
+                        // checks if all expected nodes are connected before sending flows to the new node
+                        let connected_node_count = node_ws.read().await.len();
+
+                        if let Some(expected_node_count) = config.topology.n_nodes {
+                            info!(
+                                "Connected nodes: {}/{}.",
+                                connected_node_count, expected_node_count
+                            );
+                        }
+
                         // asks the new node to connect to other nodes in the topology
 
                         // first fetches all nodes from the database
