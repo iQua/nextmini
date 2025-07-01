8f39bf2d175aaa4b8dadb742bc34fd6a81bfc18d
Xindan Zhang <xindan.zhang@mail.utoronto.ca>
2025-07-01 13:11:14 -0400

    Updated comments in build_flows_for_node function.

--- controller/src/utils.rs
+++ controller/src/utils.rs
@@ -26,11 +26,11 @@ pub fn build_startup_response(
     }
 }
 
-/// Converting DbFlow to ControllerToDataplane message.
+/// Builds a flow message for a specific node.
 pub fn build_flows_for_node(flow: DbFlow) -> ControllerToDataplane {
     debug!("Building flow message for flow id {}", flow.id);
 
-    // Convert database Flow to message Flow
+    // converts database Flow to message Flow.
     let flow_len = match flow.flow_len_type.as_str() {
         "bytes" => FlowLen::Bytes(flow.flow_len_bytes.unwrap_or(0) as usize),
         "duration" => FlowLen::Duration(flow.flow_len_duration.unwrap_or(0.0)),
