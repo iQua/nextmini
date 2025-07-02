730125a0446c5ec365a5db2b4fb6bf2c372148de
ShengwenChang <shengwen.chang@mail.utoronto.ca>
2025-07-02 03:29:01 +0000

    Reverted to raw fd map.

--- ebpf/ebpf/redirect.bpf.c
+++ ebpf/ebpf/redirect.bpf.c
@@ -1,33 +1,19 @@
 #include <linux/bpf.h>
 #include <bpf/bpf_helpers.h>
 
-// Sockhash map
+// Sockhash map, mapping raw fds
 struct {
     __uint(type, BPF_MAP_TYPE_SOCKHASH);
     __uint(max_entries, 1024);
-    __type(key, __u32);
+    __type(key, __u32); 
 } sock_hash SEC(".maps");
-
-// Map by remote port
-struct {
-    __uint(type, BPF_MAP_TYPE_HASH);
-    __uint(max_entries, 1024);
-    __type(key, __u32);    
-    __type(value, __u32);  
-} port_map SEC(".maps");
 
 SEC("sk_msg")
 int tcp_redirect(struct sk_msg_md *msg)
 {
-    __u32 src_port = msg->remote_port;  
-    __u32 *target_port;
+    __u32 current_fd = (__u32)(unsigned long)msg->sk;
     
-    target_port = bpf_map_lookup_elem(&port_map, &src_port);
-    if (!target_port) {
-        return SK_PASS;
-    }
-    
-    return bpf_msg_redirect_hash(msg, &sock_hash, target_port, BPF_F_INGRESS);
+    return bpf_msg_redirect_hash(msg, &sock_hash, &current_fd, BPF_F_INGRESS);
 }
 
 char _license[] SEC("license") = "GPL";
