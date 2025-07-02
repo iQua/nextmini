f6ebd97e0fcb75fe62d335a4a3089a2821b7dc8c
ShengwenChang <shengwen.chang@mail.utoronto.ca>
2025-07-02 03:23:16 +0000

    Changed ebpf logic to use sockhash and mapped by remote port.

--- ebpf/ebpf/redirect.bpf.c
+++ ebpf/ebpf/redirect.bpf.c
@@ -1,28 +1,33 @@
 #include <linux/bpf.h>
 #include <bpf/bpf_helpers.h>
 
-// Socket map to store socket pairs
+// Sockhash map
 struct {
-    __uint(type, BPF_MAP_TYPE_SOCKMAP);
+    __uint(type, BPF_MAP_TYPE_SOCKHASH);
     __uint(max_entries, 1024);
     __type(key, __u32);
-    __type(value, __u64);
-} sock_map SEC(".maps");
+} sock_hash SEC(".maps");
+
+// Map by remote port
+struct {
+    __uint(type, BPF_MAP_TYPE_HASH);
+    __uint(max_entries, 1024);
+    __type(key, __u32);    
+    __type(value, __u32);  
+} port_map SEC(".maps");
 
 SEC("sk_msg")
 int tcp_redirect(struct sk_msg_md *msg)
 {
-    __u32 key = msg->sk->src_port;
-    __u64 *redirect_fd;
+    __u32 src_port = msg->remote_port;  
+    __u32 *target_port;
     
-    // Look up partner socket
-    redirect_fd = bpf_map_lookup_elem(&sock_map, &key);
-    if (!redirect_fd) {
-        return SK_PASS;  // Let userspace handle
+    target_port = bpf_map_lookup_elem(&port_map, &src_port);
+    if (!target_port) {
+        return SK_PASS;
     }
     
-    // Redirect to partner socket
-    return bpf_msg_redirect_map(msg, &sock_map, *redirect_fd, BPF_F_INGRESS);
+    return bpf_msg_redirect_hash(msg, &sock_hash, target_port, BPF_F_INGRESS);
 }
 
 char _license[] SEC("license") = "GPL";
