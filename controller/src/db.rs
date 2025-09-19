58ae36588008454035fbb31d6795cb8e4a94addb
Xindan Zhang <xindan.zhang@mail.utoronto.ca>
2025-09-18 21:23:53 -0400

    Fixed log message.

--- controller/src/db.rs
+++ controller/src/db.rs
@@ -235,7 +235,7 @@ pub async fn init_db(config: &config::Config) -> Pool<Postgres> {
 
         if let Some(row) = result {
             let route_id: i32 = row.get("route_id");
-            info!("Created route_id {} from {}→{} with {} edges.", route_id, src, dst, edges.len());
+            info!("Created route_id {} from {}→{} with {} edges.", route_id, src_node_id, dst_node_id, edges.len());
         }
     }
 
