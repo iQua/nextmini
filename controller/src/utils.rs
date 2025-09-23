fdf2fcdf17d43a697efc1bbb293fe9e2678f8e46
Xindan Zhang <xindan.zhang@mail.utoronto.ca>
2025-09-23 10:31:30 -0400

    Added debug logging for routing table entries.

--- controller/src/utils.rs
+++ controller/src/utils.rs
@@ -293,6 +293,15 @@ pub fn build_routes_for_node(routes: Vec<Route>, node_id: u32) -> Option<Control
             route_entries.len()
         );
 
+        // logs each routing table entry
+        // TODO: removes this after debugging
+        for e in &route_entries {
+            info!(
+                "RoutingTableEntry node {}: route_id={} src={} dst={} next_hops={:?}",
+                node_id, e.route_id, e.src_node_id, e.dst_node_id, e.next_hops
+            );
+        }
+
         Some(ControllerToDataplane::InstallRoutes {
             routes: route_entries,
         })
