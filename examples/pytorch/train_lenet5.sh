90d026238946b8a59df45b5d9c0b829d39c8ebce
Baochun Li <bli@ece.toronto.edu>
2025-09-24 13:42:39 -0400

    Cleaned up the documentation for the PyTorch distributed trainer example in a single-machine Docker setup.

--- examples/pytorch/train_lenet5.sh
+++ examples/pytorch/train_lenet5.sh
@@ -5,5 +5,5 @@ mpirun --allow-run-as-root \
   -x MASTER_PORT=1234 \
   -x PATH \
   -bind-to none \
-  -map-by slot \
+  -map-by :OVERSUBSCRIBE \
   sh -c 'export RANK=$OMPI_COMM_WORLD_RANK; export WORLD_SIZE=$OMPI_COMM_WORLD_SIZE; uv run lenet5.py'
