669fd0517197b9704dec76914bd71b45066d4feb
Xindan Zhang <xindan.zhang@mail.utoronto.ca>
2025-09-25 17:36:33 -0400

    Added period to log message for node connection.

--- controller/src/main.rs
+++ controller/src/main.rs
@@ -495,7 +495,7 @@ async fn new_node_connected(
 
                         let duration_secs = start_instant.unwrap().elapsed().as_secs_f32();
                         info!(
-                            "All nodes connected in {:.2} seconds from first accept",
+                            "All nodes connected in {:.2} seconds from first accept.",
                             duration_secs
                         );
                     } else {
