149b161351898833a4f345435c982e216933a62b
Baochun Li <bli@ece.toronto.edu>
2025-09-25 18:08:03 -0400

    Extracted NodeConnectedEvent and new_node_connected to a separate file.

---
+++ controller/src/new_node.rs
@@ -0,0 +1,242 @@
+use std::collections::HashMap;
+use std::sync::Arc;
+use std::time::Instant;
+
+use tokio::time::Duration;
+
+use futures_util::SinkExt;
+use sqlx::{Pool, Postgres};
+use tokio::sync::{Mutex, RwLock, broadcast};
+use tokio_tungstenite::tungstenite::Message;
+use tracing::{error, info, warn};
+
+use nextmini_messages::{ControllerToDataplane, TokenBucketSpec};
+
+use crate::NodeWriterMap;
+use crate::WebSocketWriter;
+use crate::config::Config;
+use crate::models::{DbFlow, Node};
+use crate::utils::build_flows_for_node;
+
+// Event for node connection coordination
+#[derive(Debug, Clone)]
+pub struct NodeConnectedEvent {
+    pub node_id: usize,
+}
+
+/// A background task that checks if all the expected nodes have already connected.
+pub async fn new_node_connected(
+    mut event_receiver: broadcast::Receiver<NodeConnectedEvent>,
+    config: Config,
+    node_ws: Arc<RwLock<HashMap<usize, Arc<Mutex<WebSocketWriter>>>>>,
+    db_pool: Arc<Pool<Postgres>>,
+) {
+    let mut all_nodes_handled = false;
+    let mut start_time = None;
+
+    while let Ok(event) = event_receiver.recv().await {
+        if all_nodes_handled {
+            continue; // already handled all the work after all nodes connected
+        }
+
+        // sends flows and link rates when all nodes are connected
+        if let Some(expected_node_count) = config.topology.compute_node_count() {
+            let connected_node_count = node_ws.read().await.len();
+
+            if start_time.is_none() {
+                start_time = Some(Instant::now());
+                info!("The first node has connected. Starting the timer.");
+            }
+
+            if connected_node_count == expected_node_count {
+                all_nodes_handled = true;
+
+                // waits for all links to be established
+                tokio::time::sleep(Duration::from_secs(1)).await;
+
+                info!(
+                    "All {} nodes are now connected. Sending node addresses, link rates and flows to all nodes.",
+                    expected_node_count
+                );
+
+                // updates remote node addresses for the connector
+                send_node_addresses(config.clone(), node_ws.clone(), db_pool.clone()).await;
+
+                // waits for all nodes to receive the AddNode messages
+                tokio::time::sleep(Duration::from_millis(100)).await;
+                send_link_rates(config.clone(), node_ws.clone()).await;
+
+                // waits for all link rates to be set before sending the flows
+                tokio::time::sleep(Duration::from_millis(100)).await;
+                send_flows(node_ws.clone(), db_pool.clone()).await;
+
+                let duration_secs = start_time.unwrap().elapsed().as_secs_f32();
+                info!(
+                    "All dataplane nodes have connected. It takes {:.2} seconds since the first node arrived.",
+                    duration_secs
+                );
+            } else {
+                info!(
+                    "A new node with ID {} has connected. There are {} nodes already connected, out of a total of {} expected.",
+                    event.node_id, connected_node_count, expected_node_count
+                );
+            }
+        }
+    }
+}
+
+async fn send_flows(node_ws: NodeWriterMap, db_pool: Arc<Pool<Postgres>>) {
+    let db_flows: Vec<DbFlow> =
+        match sqlx::query_as("SELECT * FROM flows WHERE is_finished = false")
+            .fetch_all(&*db_pool)
+            .await
+        {
+            Ok(flows) => flows,
+            Err(e) => {
+                error!("Failed to fetch flows from database: {}.", e);
+                return;
+            }
+        };
+
+    let node_ws_guard = node_ws.read().await;
+
+    for (&node_id, writer) in node_ws_guard.iter() {
+        let flows: Vec<DbFlow> = db_flows
+            .iter()
+            .filter(|flow| flow.src_node_id == node_id as i32 || flow.dst_node_id == node_id as i32)
+            .cloned()
+            .collect();
+
+        if !flows.is_empty() {
+            info!(
+                "Adding {} user-space TCP flows to node {}.",
+                flows.len(),
+                node_id
+            );
+
+            let msg = build_flows_for_node(flows);
+
+            match writer
+                .lock()
+                .await
+                .send(Message::binary(rmp_serde::to_vec(&msg).unwrap()))
+                .await
+            {
+                Ok(_) => {
+                    info!("Successfully sent flows to node {}.", node_id);
+                }
+                Err(e) => {
+                    error!("Failed to send flows to node {}: {}.", node_id, e);
+                }
+            }
+        }
+    }
+}
+
+async fn send_link_rates(config: Config, node_ws: NodeWriterMap) {
+    let node_ws_guard = node_ws.read().await;
+
+    for (node_id, writer) in node_ws_guard.iter() {
+        let link_rates: Vec<_> = config
+            .link_rates
+            .iter()
+            .filter(|link_rate| link_rate.src_node_id == *node_id)
+            .cloned()
+            .collect();
+
+        if !link_rates.is_empty() {
+            info!("Setting link rates for node {}.", node_id);
+        }
+
+        for link_rate in link_rates {
+            let msg = ControllerToDataplane::SetLinkRate {
+                node_id: link_rate.dst_node_id,
+                spec: TokenBucketSpec {
+                    rate: link_rate.rate,
+                    bucket_size: link_rate.bucket_size,
+                },
+            };
+
+            match writer
+                .lock()
+                .await
+                .send(Message::binary(rmp_serde::to_vec(&msg).unwrap()))
+                .await
+            {
+                Ok(_) => {}
+                Err(e) => error!(
+                    "Failed to send the SetLinkRate message to node {}: {}.",
+                    link_rate.src_node_id, e
+                ),
+            }
+        }
+    }
+}
+
+async fn send_node_addresses(config: Config, node_ws: NodeWriterMap, db_pool: Arc<Pool<Postgres>>) {
+    let nodes: Vec<Node> = match sqlx::query_as("SELECT * FROM nodes")
+        .fetch_all(&*db_pool)
+        .await
+    {
+        Ok(nodes) => nodes,
+        Err(e) => {
+            error!("Failed to fetch nodes from database: {}", e);
+            return;
+        }
+    };
+
+    let node_ws_guard = node_ws.read().await;
+
+    info!(
+        "Sending AddNodeAddress messages to {} nodes.",
+        node_ws_guard.len()
+    );
+
+    for (node_id, writer) in node_ws_guard.iter() {
+        let current_node = match nodes.iter().find(|n| n.id == *node_id as i32) {
+            Some(node) => node,
+            None => {
+                warn!(
+                    "Node {} is in the writer map but not in the database. Skipping.",
+                    node_id
+                );
+                continue;
+            }
+        };
+
+        let remote_nodes: Vec<_> = nodes
+            .iter()
+            .filter(|node| node.id != *node_id as i32)
+            .collect();
+
+        for node in remote_nodes {
+            let remote_addr = if node.private_network_name == current_node.private_network_name {
+                node.private_network_addr.clone()
+            } else {
+                node.public_network_addr.clone()
+            };
+
+            // replace the port with the Tcp max server port
+            let remote_ip = remote_addr.split(':').next().unwrap();
+            let remote_addr = format!("{}:{}", remote_ip, config.max_server_port);
+
+            let msg = ControllerToDataplane::AddNodeAddress {
+                remote_node_id: node.id as usize,
+                remote_max_server_addr: remote_addr,
+            };
+
+            match writer
+                .lock()
+                .await
+                .send(Message::binary(rmp_serde::to_vec(&msg).unwrap()))
+                .await
+            {
+                Ok(_) => {}
+                Err(e) => error!(
+                    "Failed to send an AddNodeAddress message to node {}: {}.",
+                    node.id, e
+                ),
+            }
+        }
+    }
+}
