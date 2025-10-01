9dca6e33b54172f35245ab40d2e23a98d289aec3
Xindan Zhang <xindan.zhang@mail.utoronto.ca>
2025-10-01 13:35:16 -0400

    Reduced PostgreSQL connection pool size.

--- controller/src/db.rs
+++ controller/src/db.rs
@@ -200,8 +200,8 @@ pub async fn init_db(config: &config::Config) -> Pool<Postgres> {
     info!("Connecting to PostgreSQL: {}", db_url);
 
     let pool = PgPoolOptions::new()
-        .max_connections(700) // supports up to a maximum threshold of connections
-        .min_connections(600) // maintains a minimum number of connections
+        .max_connections(100) // supports up to a maximum threshold of connections
+        .min_connections(50) // maintains a minimum number of connections
         .connect(&db_url)
         .await
         .expect("Failed to connect to database");
