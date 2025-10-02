65fb171d8219c95a11aa58c8f9a60a6c6f0d29a1
Xindan Zhang <xindan.zhang@mail.utoronto.ca>
2025-10-02 14:18:09 +0000

    Revised the success message in the cleanup script.

--- examples/namespace/cleanup.sh
+++ examples/namespace/cleanup.sh
@@ -5,4 +5,4 @@ for dev in $(ip -o link show type veth | awk -F": " "{print $2}" | cut -d"@" -f1
     ip link del "$dev" 2>/dev/null || true
 done
 '
-echo "Cleaned up veths successfully"
+echo "The virtual network environment has been successfully cleaned up. You can now run the controller containers again."
