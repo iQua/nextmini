560fee216a1564d48218a34cf9238c9a1a68481d
Baochun Li <bli@ece.toronto.edu>
2025-10-02 11:57:19 -0400

    Restored retry interval and limit.

--- dataplane/src/node/namespace/manager.rs
+++ dataplane/src/node/namespace/manager.rs
@@ -134,8 +134,8 @@ impl NamespaceManager {
 
             // waits and verify that the veth pair link is established (has carrier)
             // this prevents proceeding with a node that has NO-CARRIER state
-            // retrying for up to 75 seconds, which should be sufficient even under heavy load
-            let wait_result = rt.block_on(async { wait_for_veth_carrier(veth_idx, 30, 150).await });
+            // retrying for up to 5 seconds, which should be sufficient even under heavy load
+            let wait_result = rt.block_on(async { wait_for_veth_carrier(veth_idx, 50, 100).await });
 
             if let Err(e) = wait_result {
                 error!(
