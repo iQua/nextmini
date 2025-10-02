47296f77f215e9997708954ff602a95baffbaec5
Baochun Li <bli@ece.toronto.edu>
2025-10-02 15:21:44 -0400

    Reduced default waiting time for child handshake.

--- dataplane/src/node/config.rs
+++ dataplane/src/node/config.rs
@@ -290,7 +290,7 @@ pub struct LocalConfig {
     pub child_start_delay_ms: u64,
 
     /// Timeout (ms) for parent waiting on child network setup handshake in namespace mode.
-    #[default(60000)]
+    #[default(25000)]
     #[arg(skip)]
     pub handshake_timeout_ms: u64,
 }
