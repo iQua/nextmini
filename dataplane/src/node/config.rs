bb350dc5fe0650c1e10efbdeed591abef300ca55
Baochun Li <bli@ece.toronto.edu>
2025-10-02 16:01:33 -0400

    Increased default interval_between_spawn.

--- dataplane/src/node/config.rs
+++ dataplane/src/node/config.rs
@@ -275,7 +275,7 @@ pub struct LocalConfig {
     pub subnet: u8,
 
     /// Amount of time to wait (ms) between node creation in namespace mode.
-    #[default(50)]
+    #[default(200)]
     #[arg(skip)]
     pub interval_between_spawn: u64,
 
