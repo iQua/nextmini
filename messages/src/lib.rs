0b13f51eca833b97ac9fca4d9a7081d2aa6f69dd
Xindan Zhang <xindan.zhang@mail.utoronto.ca>
2025-10-07 14:53:36 -0400

    Added ExternalFlowStart message to DataplaneToController.

--- messages/src/lib.rs
+++ messages/src/lib.rs
@@ -24,6 +24,12 @@ pub enum DataplaneToController {
     FlowFinished {
         controller_id: i32,
     },
+    ExternalFlowStart {
+        flow_id: [u8; 16],
+        src_node_id: usize,
+        dst_node_id: usize,
+        first_seen: chrono::DateTime<chrono::Utc>,
+    },
 }
 
 /// Performance metrics for a particular flow on a link from a local node to remote node.
