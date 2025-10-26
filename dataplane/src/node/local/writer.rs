2643566e8189dc668a728de68173131b5f5edff4
Baochun Li <bli@ece.toronto.edu>
2025-10-26 15:58:12 -0400

    Improved the comments in local/writer.rs.

--- dataplane/src/node/local/writer.rs
+++ dataplane/src/node/local/writer.rs
@@ -196,8 +196,8 @@ impl ConcurrentLocalWriterProducer {
                     if let Some(LocalInterfaceMessage::WritePacket(packet)) = msg {
                         let flow_id = packet.flow_id;
 
-                        // sends the packet out to the TUN device if it is not a TCP packet, or if it is SYN, FIN,
-                        // RST, or ACK
+                        // sends the packet out to the TUN device if it is not a TCP data packet, including
+                        // the cases where it is SYN, FIN, RST, or pure ACK packet
                         if !packet.is_tcp_data() {
                             let buf = &packet.buf[0..packet.packet_size];
                             if let Err(e) = self.device.send(buf).await {
@@ -207,7 +207,7 @@ impl ConcurrentLocalWriterProducer {
                             continue;
                         }
 
-                        // if it is a TCP packet, it is sequenced and stored in the queue
+                        // only when it is a TCP packet, it is reordered when needed and stored in the queue
                         let sequenced_packet = SequencedPacket { seq: packet.seq_num(), packet };
                         {
                             let mut queue_map = self.queue_map.lock().await;
@@ -218,13 +218,12 @@ impl ConcurrentLocalWriterProducer {
                             if was_empty {
                                 let mut active_flows = self.active_flows.lock().await;
                                 active_flows.insert(flow_id);
-                                // notifies the consumer immediately when a previously-empty flow gets its first pkt
-                                self.queue_not_empty.notify_one();
-                            }
 
-                            // notifies the consumer that the queue has accumulated packets beyond a threshold, so packets are
-                            // guaranteed to be consumed in a relatively ordered manner
-                            if heap.len() > self.config.reorder_tolerance {
+                                // notifies the consumer immediately when a previously-empty flow gets its first packet
+                                self.queue_not_empty.notify_one();
+                            } else if heap.len() > self.config.reorder_tolerance {
+                            // notifies the consumer after the queue has accumulated packets beyond a threshold, so packets
+                            // are guaranteed to be consumed in a relatively ordered manner
                                 self.queue_not_empty.notify_one();
                             }
                         }
