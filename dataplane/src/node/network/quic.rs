e6dc66df53b94dca484bb600af74800cf169f7a3
Baochun Li <bli@ece.toronto.edu>
2025-10-27 01:53:16 +0000

    Cargo clippy fix.

--- dataplane/src/node/network/quic.rs
+++ dataplane/src/node/network/quic.rs
@@ -994,7 +994,7 @@ mod no_cc {
             _rtt_estimator: &RttEstimator,
             _publisher: &mut Pub,
         ) -> Self::PacketInfo {
-            ()
+            
         }
         fn on_rtt_update<Pub: Publisher>(
             &mut self,
