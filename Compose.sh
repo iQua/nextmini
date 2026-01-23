eb745b0887167c93de90c3b6f6badccc1c9a4f47
Lucas <lucasgmarsh@gmail.com>
2026-01-22 20:39:58 -0600

    Auto compose installation of CC toolchain and cargo on baremetal

--- Compose.sh
+++ Compose.sh
@@ -4,8 +4,74 @@ reset
 # Set environment to current script directory
 export THIS_SHELL_SCRIPT_FULL_PATH=$(readlink -f "$0")
 export COMPOSE_PATH=$(dirname "$THIS_SHELL_SCRIPT_FULL_PATH")
-
 pushd $COMPOSE_PATH
+
+InstallCargoOnce() {
+
+    if [ ! $(command -v cargo) ]; then
+        echo "Installing cargo & rust once..."
+        curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+        echo "source $HOME/.cargo/env" >> $HOME/.bashrc
+    else
+        echo "Cargo found!"
+    fi
+
+    export PATH="/${HOME}/.cargo/bin:${PATH}"
+
+}
+
+InstallCcCompilerToolchainOnce() {
+
+    if [ ! $(command -v cmake) ]; then
+        echo "Installing CC compiler toolchain once..."
+        sudo apt-get update && \
+        sudo apt-get install -y \
+        openssh-client \
+        openssh-server \
+        build-essential \
+        cmake \
+        pkg-config && \
+        sudo apt-get clean
+
+        # Install utilities
+        sudo apt-get update && \
+        sudo apt-get install -y \
+        curl\
+        git\
+        vim \
+        net-tools && \
+        sudo apt-get clean
+    else
+        echo "CC compiler toolchain found!"
+    fi
+}
+
+ComposeDocs() {
+
+    pushd docs
+        sudo docker compose -f docker-compose.yml build --parallel
+    popd
+}
+
+ComposeController() {
+
+    pushd controller
+        sudo docker compose -f docker-compose.yml build --parallel
+    popd
+}
+
+ComposeDataplane() {
+
+    pushd dataplane
+        sudo docker compose -f docker-compose.yml build --parallel
+    popd
+}
+
+ComposeAllResources() {
+
+    InstallCcCompilerToolchainOnce
+
+    InstallCargoOnce
 
     # Build everything for coverage
     cargo run -p cert-gen
@@ -14,20 +80,14 @@ pushd $COMPOSE_PATH
     cargo build -p controller --release
     cargo build -p nextmini   --release
 
-    pushd docs
-        sudo docker compose -f docker-compose.yml build --parallel
-    popd
+    ComposeDocs
+    ComposeController
+    ComposeDataplane
 
-    pushd controller
-        sudo docker compose -f docker-compose.yml build --parallel
-    popd
-
-    pushd dataplane
-        sudo docker compose -f docker-compose.yml build --parallel
-    popd
-
+    sudo docker run -itd nextmini_docs
     sudo docker run -itd nextmini_controller ./target/release/controller
-
     sudo docker run      nextmini_datapath   ./target/release/nextmini
 
+}
+ComposeAllResources
 popd
