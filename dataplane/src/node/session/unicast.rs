68ffb41c43c2801624e865324b82b73e90455c0f
Baochun Li <bli@ece.toronto.edu>
2026-02-12 15:18:13 -0500

    Reverted unrelated files from the T11 commit.

--- dataplane/src/node/session/unicast.rs
+++ dataplane/src/node/session/unicast.rs
@@ -4,7 +4,6 @@ use std::hash::{Hash, Hasher};
 use bytes::Bytes;
 use tracing::{debug, warn};
 
-use nextmini_messages::lossless_session::FecCapabilities;
 use nextmini_messages::{Flow, FlowLen, TokenBucketSpec};
 
 use crate::node::config::LocalConfig;
@@ -112,7 +111,6 @@ impl LosslessUnicastFlowManager {
                 receiver_ids: vec![flow.dst_node_id],
                 total_bytes,
                 source_buffer,
-                fec_manifest: None,
                 ready_grace_ms: runtime_config.ready_grace_ms,
                 topology_ready: None,
             };
@@ -196,7 +194,6 @@ impl LosslessUnicastFlowManager {
                 source_node_id: flow.src_node_id,
                 expected_bytes,
                 sink_buffer: None,
-                fec_capabilities: FecCapabilities::default(),
             };
 
             // Register receiver directly with the pre-computed session_id.
