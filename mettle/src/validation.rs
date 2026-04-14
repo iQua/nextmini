a5c2a9dc248ff1d153cafdba0f0b947330c89833
Xindan Zhang <xindan.zhang@mail.utoronto.ca>
2026-04-14 12:49:19 -0400

    Tightened METTLE validation helpers.

--- mettle/src/validation.rs
+++ mettle/src/validation.rs
@@ -4,6 +4,20 @@ use crate::decoder::{DecodedSource, MettleDecoder};
 use crate::encoder::{MettleBin, MettleEncoder};
 use crate::{MettleParams, OverheadRatio};
 
+const EXPECTED_SMALL_STREAM_BINS: [(u128, [u8; 2]); 11] = [
+    (0, [1, 2]),
+    (1, [3, 4]),
+    (2, [5, 6]),
+    (332, [3, 4]),
+    (334, [5, 6]),
+    (336, [1, 2]),
+    (482, [2, 6]),
+    (494, [5, 6]),
+    (547, [1, 2]),
+    (556, [3, 4]),
+    (570, [5, 6]),
+];
+
 fn small_source_stream() -> (MettleParams, NonZeroUsize, Vec<Vec<u8>>, Vec<MettleBin>) {
     let params = MettleParams::new(OverheadRatio::new(1, 20).expect("valid overhead"));
     let source_symbol_bytes = NonZeroUsize::new(2).expect("non-zero");
@@ -76,30 +90,7 @@ fn expected_small_stream_decoded(sources: &[Vec<u8>]) -> [(u64, &[u8]); 3] {
     ]
 }
 
-fn expected_small_stream_bins() -> [(u128, [u8; 2]); 11] {
-    [
-        (0, [1, 2]),
-        (1, [3, 4]),
-        (2, [5, 6]),
-        (332, [3, 4]),
-        (334, [5, 6]),
-        (336, [1, 2]),
-        (482, [2, 6]),
-        (494, [5, 6]),
-        (547, [1, 2]),
-        (556, [3, 4]),
-        (570, [5, 6]),
-    ]
-}
-
-fn expected_kept_bins_after_erasing(bin_id: u128) -> Vec<(u128, [u8; 2])> {
-    expected_small_stream_bins()
-        .into_iter()
-        .filter(|(candidate_bin_id, _)| *candidate_bin_id != bin_id)
-        .collect()
-}
-
-fn two_byte_bin_parts(bins: impl IntoIterator<Item = MettleBin>) -> Vec<(u128, [u8; 2])> {
+fn small_stream_bin_parts(bins: impl IntoIterator<Item = MettleBin>) -> Vec<(u128, [u8; 2])> {
     bins.into_iter()
         .map(MettleBin::into_parts)
         .map(|(bin_id, payload)| {
@@ -144,19 +135,20 @@ fn validation_harness_stalls_until_a_missing_prefix_bin_is_replayed() {
 fn validation_harness_matches_small_stream_bin_goldens() {
     let (_, _, _, bins) = small_source_stream();
 
-    assert_eq!(
-        two_byte_bin_parts(bins),
-        expected_small_stream_bins()
-    );
+    assert_eq!(small_stream_bin_parts(bins), EXPECTED_SMALL_STREAM_BINS);
 }
 
 #[test]
 fn validation_harness_matches_kept_bin_goldens_after_prefix_erasure() {
     let (params, _, _, bins) = small_source_stream();
     let (kept_bins, _) = split_out_bin(bins, params.tle_bin_id(0));
+    let expected = EXPECTED_SMALL_STREAM_BINS
+        .into_iter()
+        .filter(|(bin_id, _)| *bin_id != params.tle_bin_id(0))
+        .collect::<Vec<_>>();
 
     assert_eq!(
-        two_byte_bin_parts(kept_bins),
-        expected_kept_bins_after_erasing(params.tle_bin_id(0))
+        small_stream_bin_parts(kept_bins),
+        expected
     );
 }
